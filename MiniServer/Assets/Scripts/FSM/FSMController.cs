52f92bf138b903ce53caf5e46d36306a879026f0
jccg891113 <sachuan@foxmail.com>
2015-11-14 15:34:55 +0800

    添加状态机基类

---
+++ MiniServer/Assets/Scripts/FSM/FSMController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fsm;
+
+namespace Fsm.JFSM
+{
+	public interface IFSMController<T> where T : struct {
+		
+	}
+	
+	public class FSMController<T> : IFSMController<T> where T : struct {
+		protected FSMMachine<T> machine;
+	}
+	
+	public class FSMMonoController<T> : MonoBehaviour, IFSMController<T> where T : struct {
+		protected FSMMachine<T> machine;
+		
+		public virtual void Update () {
+			if (machine != null) {
+				machine.UpdateState(Time.deltaTime);
+			}
+		}
+	}
+}
---
+++ MiniServer/Assets/Scripts/FSM/FSMState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fsm;
+
+namespace Fsm.JFSM
+{
+	public class FSMState<T> where T : struct {
+		
+		protected readonly T stateType;
+		protected readonly IFSMController<T> controller;
+		
+		public FSMState(T stateType, IFSMController<T> controller) {
+			this.stateType = stateType;
+			this.controller = controller;
+		}
+		
+		public virtual void Enter()
+		{
+			
+		}
+		
+		public virtual void Leave()
+		{
+			
+		}
+		
+        public virtual void Update (float deltaTime) {
+        
+        }
+		
+		public T GetStateType()
+		{
+			return stateType;
+		}
+		
+		public virtual T GetNextState () {
+			return stateType;
+		}
+		
+	}
+}
